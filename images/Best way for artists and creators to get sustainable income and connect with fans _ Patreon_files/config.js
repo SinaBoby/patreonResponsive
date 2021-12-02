fc0318b17fd4bdc1b106f04deac6fa6d4a84e36d
Sina Boby <sinaboby91@yahoo.com>
2021-12-02 01:12:19 +0100

    Till gallery edited

---
+++ images/Best way for artists and creators to get sustainable income and connect with fans _ Patreon_files/config.js
@@ -0,0 +1,5 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C65HD29R48GN82KJEGU0").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true});
+window[window["TiktokAnalyticsObject"]].instance("C65HD29R48GN82KJEGU0").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C65HD29R48GN82KJEGU0").setPixelInfo({status: 0, name: "Patreon TikTok Pixel", advertiserID: "7008638140088909826", setupMode: 1, partner: "", is_onsite: false});
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("patreon.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
